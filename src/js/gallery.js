04ce439070a04d7b4a42d61d772cca0ee44c3a59
Olga Tolstykhina <ovt0709@gmail.com>
2023-03-18 00:10:38 +0200

    start project

---
+++ src/js/gallery.js
@@ -0,0 +1,10 @@
+import axios, { isCancel, AxiosError } from 'axios';
+import Notiflix from 'notiflix';
+// e.g. Only message
+Notiflix.Notify.success('Sol lucet omnibus');
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+Notiflix.Notify.warning('Memento te hominem esse');
+Notiflix.Notify.info('Cogito ergo sum');
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
