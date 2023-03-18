35fe1c2c2b314a4a7b146ac0b252b8c54578e9c5
Olga Tolstykhina <ovt0709@gmail.com>
2023-03-18 04:37:52 +0200

    add HBS

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,5 @@
 import notifier from './notifler'
-
+import markupTpl from '../partials/markup.hbs';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css"
 
@@ -11,15 +11,20 @@ const refs = {
     
 };
 
+
+// HTTP-запити виконуються при введенні назви після Сабміт
 refs.searchForm.addEventListener('submit', onSearchSubmit);
 
 function onSearchSubmit(event) {
     event.preventDefault();
-    refs.galleryContainer.innerHTML = ''
-    const photo = event.currentTarget.elements.searchQuery.value;
-
+    refs.galleryContainer.innerHTML = '' // стартово прибираємо розмітку
+    const photo = event.currentTarget.elements.searchQuery.value.trim(); // прибираються зайві пробіли
+    if (!photo) {
+        return; // якщо пустая строка, запит не відправляється
+            }
     fetchPhoto(photo)  
         .then(photo => {
+            
             const galleryItems = photo.hits;
             if (photo.hits.length === 0) {
                 notifier.warning("Sorry, there are no images matching your search query. Please try again.")
@@ -29,29 +34,9 @@ function onSearchSubmit(event) {
             refs.galleryContainer.insertAdjacentHTML('beforeend', createGalary(galleryItems))
 
             function createGalary(galleryItems) {
-                return galleryItems.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-                    return `
-                    <div class="photo-card">
-                        <a class="gallery__item" href="${largeImageURL}">
-                            <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-                        </a>
-                        <div class="info">
-                            <p class="info-item">
-                            <b>Likes </b></br> ${likes}
-                            </p>
-                            <p class="info-item">
-                            <b>Views </b></br> ${views}
-                            </p>
-                            <p class="info-item">
-                            <b>Comments </b></br> ${comments}
-                            </p>
-                            <p class="info-item">
-                            <b>Downloads </b></br> ${downloads}
-                            </p>
-                        </div>
-                    </div>
-                     `;
-                }).join('');
+                return galleryItems.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
+                    markupTpl({ webformatURL, largeImageURL, tags, likes, views, comments, downloads })
+                ).join('');
             };
 
             // створюємо модалку і передаємо велику картинку 
