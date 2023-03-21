826a67eb22bdb65f04394fadbf2a4d1637571c34
OlgaT0709 <113298274+OlgaT0709@users.noreply.github.com>
2023-03-21 00:38:17 +0000

    Deploying to gh-pages from @ OlgaT0709/goit-js-hw-11@c7c0ea15fe56070466ce9d98a6c0e99f83fa6fe3 🚀

---
+++ gallery1.2c8f0986.js
@@ -0,0 +1,2 @@
+(()=>{const{height:e}=document.querySelector(".page-header").getBoundingClientRect();document.body.style.paddingTop=`${e}px`})();
+//# sourceMappingURL=gallery1.2c8f0986.js.map
---
+++ gallery1.888ad9d8.js
@@ -0,0 +1,2 @@
+(()=>{const{height:e}=document.querySelector(".page-header").getBoundingClientRect();document.body.style.paddingTop=`${e}px`})();
+//# sourceMappingURL=gallery1.888ad9d8.js.map
